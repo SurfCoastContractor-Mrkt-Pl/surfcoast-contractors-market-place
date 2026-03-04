f45d5cf4cea7bb0d2d711d880f049a2341abc365
Hector Navarrete <hexthegreat25@gmail.com>
2026-03-04 21:53:47 +0000

    File changes

--- src/components/scopeofwork/JobCloseout.jsx
+++ src/components/scopeofwork/JobCloseout.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQueryClient } from '@tanstack/react-query';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
