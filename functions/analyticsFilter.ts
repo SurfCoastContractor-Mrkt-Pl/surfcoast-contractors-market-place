4cc190a46d972382c760e0a71c97b234d81b461e
Hector Navarrete <hexthegreat25@gmail.com>
2026-03-04 22:30:26 +0000

    File changes

--- functions/analyticsFilter.ts
+++ functions/analyticsFilter.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 const EXCLUDED_EMAILS = [
   'hexthegreat25@gmail.com',
