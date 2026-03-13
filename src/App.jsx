70f8b4cf51fdaa1fcdc3ccad049a83c9911eb34a
Hector Navarrete <hexthegreat25@gmail.com>
2026-03-13 00:16:23 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/AdminPreview" element={<LayoutWrapper currentPageName="AdminPreview"><AdminPreview /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
